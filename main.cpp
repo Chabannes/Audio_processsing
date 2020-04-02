34b3dc15d00c12dcf9e34a22cf98c16b3525211d
francois <chabannes.francois@gmail.com>
2020-04-02 17:22:04 +0200

    commit 3

--- main.cpp
+++ main.cpp
@@ -7,44 +7,41 @@
 int main()
 {
 
+    FILE* fileIn = fopen("europa.raw", "rb");
+    FILE* fileOut = fopen("output", "wb");
 
-    // FILE *pfile = fopen("europa.raw", "rb");
-    FILE *pipeout;
-    pipeout = popen("ffmpeg -y -f s16le -ar 44100 -ac 1 -i - out.wav", "w");
-
-    FILE * pFile;
     long lSize;
     char * buffer;
-    size_t result;
+    size_t result1;
     size_t result2;
 
-    pFile = fopen ( "europa.raw" , "rb" );
-    if (pFile==NULL) {fputs ("File error",stderr); exit (1);}
+    if (fileIn==NULL) {fputs ("File error",stderr); exit (1);}
 
     // obtain file size:
-    fseek (pFile , 0 , SEEK_END);
-    lSize = ftell (pFile);
-    rewind (pFile);
+    fseek (fileIn , 0 , SEEK_END);
+    lSize = ftell (fileIn);
+    rewind (fileIn);
 
     // allocate memory to contain the whole file:
     buffer = (char*) malloc (sizeof(char)*lSize);
+    std::cout << sizeof(*buffer) << " " << sizeof(char)*lSize << std::endl;
     if (buffer == NULL) {fputs ("Memory error",stderr); exit (2);}
 
     // copy the file into the buffer:
-    result = fread (buffer,1,lSize,pFile);
-    if (result != lSize) {fputs ("Reading error",stderr); exit (3);}
-    /* the whole file is now loaded in the memory buffer. */
+    result1 = fread (buffer, 1, lSize, fileIn);
+    if (result1 != lSize) {fputs ("Reading error",stderr); exit (3);}
 
-    // std::cout << "\n\n" << buffer << "\n\n";
-    std::cout << "\n\n" << result << "\n\n";
+    std::cout << "\n\n" << result1 << "\n\n";
 
     // copy the buffer in out.raw
-    result2 = fwrite(buffer,  sizeof(char), sizeof(buffer), pipeout);
-    std::cout << result << std::endl;
+    result2 = fwrite(buffer,  sizeof(char), lSize, fileOut);
+    std::cout << result2 << std::endl;
+    if (result2 != lSize) {fputs ("Writing error",stderr); exit (4);}
 
 
     // terminate
-    fclose (pFile);
+    fclose (fileIn);
+    fclose (fileOut);
     free (buffer);
 
 }
